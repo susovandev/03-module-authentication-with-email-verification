5896fcd12584ca82e1acd766957f02ecad512f23
Susovan Dev <susovandas.dev@gmail.com>
2025-11-18 20:03:07 +0530

    chore(server):configure Winston logger for centralized logging

--- app/db/db.connection.ts
+++ app/db/db.connection.ts
@@ -1,11 +1,12 @@
 import mongoose from 'mongoose';
 import envConfig from '../config/env.config';
+import Logger from '../utils/logger.utils';
 const connectDB = async () => {
 	try {
 		const connectionInstance = await mongoose.connect(envConfig.DB.URI);
-		console.log(`MongoDB connected: ${connectionInstance.connection.host}`);
+		Logger.info(`MongoDB connected: ${connectionInstance.connection.host}`);
 	} catch (error) {
-		console.error('Error connecting to MongoDB:', error);
+		Logger.error('Error connecting to MongoDB:', error);
 		throw error;
 	}
 };
--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,7 @@
 import app from './app';
 import envConfig from './config/env.config';
 import connectDB from './db/db.connection';
+import Logger from './utils/logger.utils';
 
 const env = envConfig.SERVER.NODE_ENV;
 const port = envConfig.SERVER.PORT;
@@ -8,10 +9,10 @@ const port = envConfig.SERVER.PORT;
 connectDB()
 	.then(() => {
 		app.listen(port, () => {
-			console.log(`Server running in ${env} mode on http://localhost:${port}`);
+			Logger.info(`Server running in ${env} mode on http://localhost:${port}`);
 		});
 	})
 	.catch((error) => {
-		console.error('Error connecting to MongoDB:', error);
+		Logger.error('Error connecting to MongoDB:', error);
 		process.exit(1);
 	});
