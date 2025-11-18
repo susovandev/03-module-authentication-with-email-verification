39a8c5962ea892bfb9422b64b2f265121aa35cb4
Susovan Dev <susovandas.dev@gmail.com>
2025-11-18 20:27:27 +0530

    feat(user): implement user schema with types, model setup and DB indexing

---
+++ app/modules/user/user.model.ts
@@ -0,0 +1,55 @@
+import { Document, Schema, model } from 'mongoose';
+
+export type IUserRole = 'user' | 'admin';
+export interface IUserDocument extends Document {
+	name: string;
+	email: string;
+	password: string;
+	isEmailVerified: boolean;
+	otp: string;
+	otpExpiry: Date;
+	role: IUserRole;
+	createdAt: Date;
+	updatedAt: Date;
+}
+
+const userSchema = new Schema<IUserDocument>(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+			lowercase: true,
+		},
+		password: {
+			type: String,
+			required: true,
+			select: false,
+		},
+		isEmailVerified: {
+			type: Boolean,
+			default: false,
+		},
+		otp: {
+			type: String,
+		},
+		otpExpiry: {
+			type: Date,
+		},
+		role: {
+			type: String,
+			enum: ['user', 'admin'],
+			default: 'user',
+		},
+	},
+	{ timestamps: true },
+);
+
+userSchema.index({ email: 1 }, { unique: true });
+const userModel = model<IUserDocument>('User', userSchema);
+
+export default userModel;
